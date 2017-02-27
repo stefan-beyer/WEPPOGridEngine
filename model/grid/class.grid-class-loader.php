af28313ce7f4f3cb466a5bc7cbaea9e383e7aad6
stefan-beyer <info@wapplications.net>
2017-02-27 22:11:57 +0100

    Add files via upload

---
+++ model/grid/class.grid-class-loader.php
@@ -0,0 +1,13 @@
+<?php
+
+define('GRID_MODEL_PATH', dirname(__FILE__));
+
+require_once GRID_MODEL_PATH . '/inc/grid.inc.php';
+
+require_once GRID_MODEL_PATH . '/model_classes/class.gridcell.php';
+require_once GRID_MODEL_PATH . '/model_classes/class.gridrow.php';
+require_once GRID_MODEL_PATH . '/model_classes/class.gridcontentparam.php';
+require_once GRID_MODEL_PATH . '/model_classes/class.gridcontent.php';
+require_once GRID_MODEL_PATH . '/model_classes/class.gridlayout.php';
+require_once GRID_MODEL_PATH . '/model_classes/class.gridtemplate.php';
+
